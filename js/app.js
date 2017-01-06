a1f4030216e181e2d01062d0c9eabf53f2f144d6
Vortex83 <hadokoda@yahoo.com>
2017-01-06 11:42:34 +0000

    still migrating to package managers

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
-import React from './react/react'
-import ReactDOM from './react/react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Menu from './components/menu'
 module.exports = function (renderer) {
-
-
     ReactDOM.render(
         <Menu />,
         document.getElementById('menu')
